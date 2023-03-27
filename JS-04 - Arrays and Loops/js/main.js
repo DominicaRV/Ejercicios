dcb605dd8ca8e0903851b5d4775e01a68ef70f51
Dominica <dominicaruizvarela@gmail.com>
2023-03-27 17:16:48 -0600

    Se agrega la parte 2

--- JS-04 - Arrays and Loops/js/main.js
+++ JS-04 - Arrays and Loops/js/main.js
@@ -7,3 +7,10 @@ nombres.splice(1, 0, "Renata")
 nombres.push("Elena")
 
 console.log(nombres)
+for (let i = 1; i <= 5; i++) {
+    let linea = "";
+    for (let j = 1; j <= i; j++) {
+        linea += "* ";
+    }
+    console.log(linea);
+}
