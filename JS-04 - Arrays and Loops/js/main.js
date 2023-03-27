434996941b827921c54d07ce6c54beece9fedc61
Dominica <dominicaruizvarela@gmail.com>
2023-03-27 17:18:45 -0600

    Se agrega la parte 3

--- JS-04 - Arrays and Loops/js/main.js
+++ JS-04 - Arrays and Loops/js/main.js
@@ -14,3 +14,27 @@ for (let i = 1; i <= 5; i++) {
     }
     console.log(linea);
 }
+let xvalue = 10;
+for (let i = xvalue; i >= 0; i -= 0.5) {
+    console.log(i);
+}
+
+let sum = 0;
+for (let i = 1; i <= 100; i++) {
+    sum += (i % 2 !== 0) ? i : 0;
+}
+console.log(`La suma de los numero impares es: ${sum}`);
+
+let cifraImprimir = 6, contador = 1;
+while (contador <= cifraImprimir) {
+    console.log(contador)
+    contador++;
+}
+
+
+let n = 19, cont = 1, suma = 0;
+while (cont <= n) {
+    suma += cont;
+    cont++;
+}
+console.log(` La suma de los numeros enteros positivos es: ${suma}`)
